b5cf19b1f664e646eaaffa577e4fa32ac59efeb6
arch-mania <98379199+arch-mania@users.noreply.github.com>
2023-11-15 00:35:32 +0900

    change contactのNetlify Functionがローカルで正常実行確認

--- netlify/functions/contact.ts
+++ netlify/functions/contact.ts
@@ -3,118 +3,118 @@ import nodemailer from "nodemailer";
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, OPTION",
-  "Access-Control-Allow-Headers":
-    "Content-Type, Access-Control-Allow-Headers, X-Requested-With",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
 };
 
 const handler: Handler = async (event) => {
-  if (event.httpMethod == "OPTIONS") {
-    console.log("IF OPTIONS");
+  if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTION",
-      },
+      headers,
+      body: JSON.stringify({ message: "Successful preflight call." }),
     };
-  }
+  } else if (event.httpMethod === "POST") {
+    try {
+      if (!event.body) {
+        return {
+          statusCode: 403,
+          body: JSON.stringify("body is required"),
+        };
+      }
 
-  try {
-    console.log(event.httpMethod);
-    // 送信用アカウントの設定（ここではGmail）
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "mail.red.shared-server.net",
-      auth: {
-        user: "mail@ageokogyo.com",
-        pass: `C40kZhnK`,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-      secure: true,
-    });
+      const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "mail.red.shared-server.net",
+        auth: {
+          user: "mail@ageokogyo.com",
+          pass: `C40kZhnK`,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+        secure: true,
+      });
 
-    if (!event.body) {
-      return {
-        statusCode: 403,
-        body: JSON.stringify("body is required"),
-      };
-    }
+      const req = JSON.parse(event.body);
 
-    const req = JSON.parse(event.body);
-
-    // 管理人に送るお問い合わせメッセージ通知メール
-    const toHostMailData = {
-      from: "mail@ageokogyo.com",
-      to: "mail@ageokogyo.com",
-      subject: `【お問い合わせ】${req.body.name}様より`,
-      html: `
+      // 管理人に送るお問い合わせメッセージ通知メール
+      const toHostMailData = {
+        from: "mail@ageokogyo.com",
+        to: "mail@ageokogyo.com",
+        subject: `【お問い合わせ】${req.name}様より`,
+        html: `
       <p>
         以下の内容でお問い合わせを承りました。
       </p>
       <p>-----------------------------------------</p>
       <h2>お問い合わせ内容</h2>
       <p>【問い合わせ種別】</p>
-      <p>${req.body.contactKinds}</p>
+      <p>${req.contactKinds}</p>
       <p>【お名前】</p>
-      <p>${req.body.name}</p>
+      <p>${req.name}</p>
       <p>【お名前（フリガナ）】</p>
-      <p>${req.body.ruby}</p>
+      <p>${req.ruby}</p>
       <p>【メールアドレス】</p>
-      <p>${req.body.email}</p>
+      <p>${req.email}</p>
       <p>【お問い合わせ内容】</p>
-      <p>${req.body.content}</p>
+      <p>${req.content}</p>
       <p>-----------------------------------------</p>
     `,
-    };
+      };
 
-    // ゲストに送る自動受付メール
-    const toGuestMailData = {
-      from: "mail@ageokogyo.com",
-      to: req.email,
-      subject: `【お問い合わせ自動受付メール】`,
-      html: `
-          <p>
-            お問い合わせありがとうございます。
-            <br>以下の内容でお問い合わせを承りました。
-          </p>
-          <p>-----------------------------------------</p>
-          <h2>お問い合わせ内容</h2>
-          <p>【問い合わせ種別】</p>
-          <p>${req.body.contactKinds}</p>
-          <p>【お名前】</p>
-          <p>${req.body.name}</p>
-          <p>【お名前（フリガナ）】</p>
-          <p>${req.body.ruby}</p>
-          <p>【メールアドレス】</p>
-          <p>${req.body.email}</p>
-          <p>【お問い合わせ内容】</p>
-          <p>${req.body.content}</p>
-          <p>-----------------------------------------</p>
-        `,
-    };
+      // ゲストに送る自動受付メール
+      const toGuestMailData = {
+        from: "mail@ageokogyo.com",
+        to: req.email,
+        subject: `【お問い合わせ自動受付メール】`,
+        html: `
+            <p>
+              お問い合わせありがとうございます。
+              <br>以下の内容でお問い合わせを承りました。
+            </p>
+            <p>-----------------------------------------</p>
+            <h2>お問い合わせ内容</h2>
+            <p>【問い合わせ種別】</p>
+            <p>${req.contactKinds}</p>
+            <p>【お名前】</p>
+            <p>${req.name}</p>
+            <p>【お名前（フリガナ）】</p>
+            <p>${req.ruby}</p>
+            <p>【メールアドレス】</p>
+            <p>${req.email}</p>
+            <p>【お問い合わせ内容】</p>
+            <p>${req.content}</p>
+            <p>-----------------------------------------</p>
+          `,
+      };
 
-    const [hostInfo, guestInfo] = await Promise.all([
-      transporter.sendMail(toHostMailData),
-      transporter.sendMail(toGuestMailData),
-    ]);
+      const [hostInfo, guestInfo] = await Promise.all([
+        transporter.sendMail(toHostMailData),
+        transporter.sendMail(toGuestMailData),
+      ]);
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTION",
-      },
-      body: JSON.stringify({ hostInfo, guestInfo }),
-    };
-  } catch (error) {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "GET, POST, OPTION",
+        },
+        body: JSON.stringify({ hostInfo, guestInfo }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify({ error: error.message }),
+      };
+    }
+  } else {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
+      headers,
+      body: JSON.stringify("prohibit method"),
     };
   }
 };
